7db7920fdac0811574668f9c9a7b9180f31d650f
Kevin Arthur <thetoothpick@gmail.com>
2022-10-15 13:01:29 -0700

    change config entry title to serial number

--- custom_components/span_panel/config_flow.py
+++ custom_components/span_panel/config_flow.py
@@ -133,7 +133,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             _LOGGER.exception("Unexpected exception")
             errors["base"] = "unknown"
         else:
-            return self.async_create_entry(title=info["title"], data=user_input)
+            return self.async_create_entry(title=info.serial_number, data=user_input)
 
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
