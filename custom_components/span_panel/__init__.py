e6382111ec0ef46cf7d8e16d4c6713ae2dd483eb
Wez Furlong <wez@wezfurlong.org>
2022-12-10 02:28:04 +0000

    fix exception import
    We error out due to the problem in
    https://github.com/galak/span-hacs/issues/7,
    but then fail to re-raise as the right exception type.

--- custom_components/span_panel/__init__.py
+++ custom_components/span_panel/__init__.py
@@ -12,6 +12,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryAuthFailed
 from homeassistant.helpers.httpx_client import get_async_client
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
